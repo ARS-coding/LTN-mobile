1266df8df899134323e5b5559c23f92f76c281d8
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-25 01:57:31 +0300

    Fix the binding of arguments error by changing the name

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Button from "./components/Button";
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button title="wassup" />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
