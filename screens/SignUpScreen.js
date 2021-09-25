b9db61384d53e8fc5609c6968454476ca19726f1
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-26 00:20:46 +0300

    Add the first name, last name and email inputs with the sign up header

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { InputField } from '../components';
 
 export default function SignUpScreen() {
+    
+    const [formData, setFormData] = useState({ 
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    });
+    const [passwordVisibility, setPasswordVisibility] = useState(false);
+    const [rightIcon, setRightIcon] = useState("eye");
+    const [signUpError, setSignUpError] = useState("");
+
     return (
-        <View>
-            <Text>SignUpScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Sign Up</Text>
+            <View style={{ height: "60%", marginTop: 30, backgroundColor: "white" }}>
+                <View style={{flexDirection: "row"}}>
+                    <InputField
+                        containerStyle={{ flex: 1, marginBottom: 20, backgroundColor: "white" }}
+                        inputStyle={{ marginLeft: 12, fontSize: 14 }}
+                        placeholder="First Name"
+                        onChangeText={(text) => setFormdata({ ...formData, firstName: text })}
+                        value={formData.firstName}
+                    />
+                    <InputField
+                        containerStyle={{ flex: 1, marginBottom: 20, marginLeft: 8, backgroundColor: "white" }}
+                        inputStyle={{ marginLeft: 12, fontSize: 14 }}
+                        placeholder="Last Name"
+                        onChangeText={(text) => setFormdata({ ...formData, lastName: text })}
+                        value={formData.lastName}
+                    />
+                </View>
+                
+                <InputField
+                    containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
+                    inputStyle={{ fontSize: 14 }}
+                    placeholder="Email"
+                    leftIcon="email"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    autoFocus={true}
+                    onChangeText={(text) => setFormdata({ ...formData, email: text })}
+                    value={formData.email}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e93b81",
+        paddingHorizontal: 12,
+        paddingTop: 40,
+    },
+    title: {
+        alignSelf: "center",
+        fontSize: 24,
+        fontWeight: "700",
+        color: "white",
+    }
+})
