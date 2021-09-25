755ec33363c2bcaccc6f7eea3502b5e685c84419
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-25 11:18:12 +0300

    Add the basic structure for signup screen

---
+++ screens/SignUpScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { View, Text } from "react-native";
+
+export default function SignUpScreen() {
+    return (
+        <View>
+            <Text>SignUpScreen</Text>
+        </View>
+    )
+}
