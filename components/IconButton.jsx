27875e89367c28429717cde253b0ee04eea1673f
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-25 14:13:48 +0300

    Add the icon button component

---
+++ components/IconButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import { Pressable, StyleSheet } from "react-native";
+
+import { AntDesign } from "@expo/vector-icons";
+
+export default function IconButton({ color, size, onPress, name }) {
+    return (
+        <Pressable
+            style={args => {
+                if (args.pressed) {
+                    return [styles.base, { opacity: .5 }]
+                };
+                return [styles.base, { opacity: 1 }];
+            }}
+            onPress={onPress}
+        >
+            <AntDesign name={name} size={size} color={color} />
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    base: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "transparent"
+    }
+});
