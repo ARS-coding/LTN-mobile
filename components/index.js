ce26b9fb74f1454944839a7be179793b5227e634
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-25 15:33:52 +0300

    Update the exports of components/index.js file

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Button from "./Button";
+import IconButton from "./IconButton";
 import InputField from "./InputField";
 import ErrorMessage from "./ErrorMessage";
 
-export { Button, InputField, ErrorMessage };
+export { Button, IconButton, InputField, ErrorMessage };
