dac1dc4e2a92f85e5370803075b9108dfa8a17c1
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-27 23:04:29 +0300

    Wrap the root navigator in navigation container component that comes from @react-navigation/native

---
+++ navigation/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { AuthenticatedUserProvider } from './AuthenticatedUserProvider';
+
+import RootNavigator from './RootNavigator';
+
+export default function Routes() {
+    return (
+        <AuthenticatedUserProvider>
+            <RootNavigator />
+        </AuthenticatedUserProvider>
+    )
+}
