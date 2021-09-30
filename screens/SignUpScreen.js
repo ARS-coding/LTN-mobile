37611cabf0078abe1eeb1cf277bdeaf509474d69
Ali Rıza <ars.lifestyle@hotmail.com>
2021-09-30 15:27:18 +0300

    Dismiss the keyboard when clicked to a place that doesn't have a onpress event

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react'
 
-import { View, Text, Button as RNButton, StyleSheet } from "react-native";
+import {
+    View,
+    Text,
+    Button as RNButton,
+    StyleSheet,
+    TouchableWithoutFeedback, 
+    Keyboard } from "react-native";
 import { InputField, Button, ErrorMessage } from '../components';
 
 import { auth } from "../config/firebase";
@@ -38,75 +44,77 @@ export default function SignUpScreen({ navigation }) {
     }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>
-                Create a{" "}
-                <Text style={{textDecorationLine: "underline"}}>
-                    New
-                </Text> 
-                {" "}Account!
-            </Text>
-            <View style={{ marginTop: 30 }}>
-                <View style={{flexDirection: "row"}}>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Create a{" "}
+                    <Text style={{textDecorationLine: "underline"}}>
+                        New
+                    </Text> 
+                    {" "}Account!
+                </Text>
+                <View style={{ marginTop: 30 }}>
+                    <View style={{flexDirection: "row"}}>
+                        <InputField
+                            containerStyle={{ flex: 1, marginBottom: 20, backgroundColor: "white" }}
+                            inputStyle={{ marginLeft: 15, fontSize: 14 }}
+                            placeholder="First Name"
+                            autoFocus={true}
+                            onChangeText={(text) => setFormData({ ...formData, firstName: text })}
+                            value={formData.firstName}
+                        />
+                        <InputField
+                            containerStyle={{ flex: 1, marginBottom: 20, marginLeft: 8, backgroundColor: "white" }}
+                            inputStyle={{ marginLeft: 15, fontSize: 14 }}
+                            placeholder="Last Name"
+                            onChangeText={(text) => setFormData({ ...formData, lastName: text })}
+                            value={formData.lastName}
+                        />
+                    </View>
+                    
                     <InputField
-                        containerStyle={{ flex: 1, marginBottom: 20, backgroundColor: "white" }}
-                        inputStyle={{ marginLeft: 15, fontSize: 14 }}
-                        placeholder="First Name"
-                        autoFocus={true}
-                        onChangeText={(text) => setFormData({ ...formData, firstName: text })}
-                        value={formData.firstName}
+                        containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
+                        inputStyle={{ fontSize: 14 }}
+                        placeholder="Email"
+                        leftIcon="email"
+                        autoCapitalize="none"
+                        keyboardType="email-address"
+                        
+                        onChangeText={(text) => setFormData({ ...formData, email: text })}
+                        value={formData.email}
                     />
                     <InputField
-                        containerStyle={{ flex: 1, marginBottom: 20, marginLeft: 8, backgroundColor: "white" }}
-                        inputStyle={{ marginLeft: 15, fontSize: 14 }}
-                        placeholder="Last Name"
-                        onChangeText={(text) => setFormData({ ...formData, lastName: text })}
-                        value={formData.lastName}
+                        containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
+                        inputStyle={{ fontSize: 14 }}
+                        placeholder="Password"
+                        leftIcon="lock"
+                        rightIcon={rightIcon}
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        secureTextEntry={passwordVisibility}
+                        rightIconOnPress={handlePasswordVisibility}
+                        
+                        onChangeText={(text) => setFormData({ ...formData, password: text })}
+                        value={formData.password}
                     />
                 </View>
-                
-                <InputField
-                    containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
-                    inputStyle={{ fontSize: 14 }}
-                    placeholder="Email"
-                    leftIcon="email"
-                    autoCapitalize="none"
-                    keyboardType="email-address"
-                    
-                    onChangeText={(text) => setFormData({ ...formData, email: text })}
-                    value={formData.email}
-                />
-                <InputField
-                    containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
-                    inputStyle={{ fontSize: 14 }}
-                    placeholder="Password"
-                    leftIcon="lock"
-                    rightIcon={rightIcon}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    secureTextEntry={passwordVisibility}
-                    rightIconOnPress={handlePasswordVisibility}
-                    
-                    onChangeText={(text) => setFormData({ ...formData, password: text })}
-                    value={formData.password}
-                />
+                {(signUpError !== "") && <ErrorMessage
+                    error={signUpError}
+                    visible={true}
+                />}
+                <View>
+                    <Button 
+                        title="Sign Up"
+                        containerStyle={{ marginBottom: 10 }}
+                        onPress={handleSignUp}    
+                    />
+                    <RNButton 
+                        title="Go to Login" 
+                        onPress={() => navigation.navigate('Login')}
+                    />
+                </View>
             </View>
-            {(signUpError !== "") && <ErrorMessage
-                error={signUpError}
-                visible={true}
-            />}
-            <View>
-                <Button 
-                    title="Sign Up"
-                    containerStyle={{ marginBottom: 10 }}
-                    onPress={handleSignUp}    
-                />
-                <RNButton 
-                    title="Go to Login" 
-                    onPress={() => navigation.navigate('Login')}
-                />
-            </View>
-        </View>
+        </TouchableWithoutFeedback>
     )
 }
 
