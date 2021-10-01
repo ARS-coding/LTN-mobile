3687a67a335906cd0296aface830d313a9b6c18f
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-01 16:51:54 +0300

    Create the store

---
+++ redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux";
+import { thunkMiddleware } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import userReducer from "./slices/userSlice";
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+export const store = createStore(userReducer, composedEnhancer);
