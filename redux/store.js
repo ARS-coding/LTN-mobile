bd1fdf02114d24565269fe5d5df494063b335bf6
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-01 17:02:53 +0300

    Fix the error by importing the thunkMiddleware without curly braces

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 
-import { thunkMiddleware } from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import userReducer from "./slices/userSlice";
