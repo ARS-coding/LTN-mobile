23ba1f8c1aa19d069c0d7657034d715a6a950ccb
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-01 23:21:30 +0300

    Change the file extension of App from js to jsx

---
+++ App.jsx
@@ -0,0 +1,48 @@
+import 'react-native-gesture-handler';
+
+import React, { useEffect } from 'react';
+import { useDispatch, Provider } from 'react-redux';
+import { listenForAuthChanges } from './redux/slices/userSlice';
+import { store } from "./redux/store";
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, NativeModules, Platform, LogBox } from 'react-native';
+
+import Routes from './navigation/RootNavigator';
+import { enableScreens } from 'react-native-screens';
+
+
+const { StatusBarManager } = NativeModules;
+const statusBarHeight = (Platform.OS === "android" && StatusBarManager.HEIGHT) || (Platform.OS === "ios" && StatusBarManager.getHeight(height => height));
+
+export default function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+
+function App() {
+  const dispatch = useDispatch()
+  LogBox.ignoreLogs(['Setting a timer']);
+  
+  useEffect(() => {
+    dispatch(listenForAuthChanges());
+    enableScreens();
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <Routes />  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: statusBarHeight,
+  },
+});
