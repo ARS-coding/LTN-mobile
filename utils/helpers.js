eb7da04f8d532d315a745598efcfeff096c43cad
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-01 18:35:21 +0300

    Add some helper functions

---
+++ utils/helpers.js
@@ -0,0 +1,40 @@
+import { firestore } from "../config/firebase";
+
+import PPMaleSVG from "../images/Profile/PPMaleSVG.svg";
+import PPFemaleSVG from "../images/Profile/PPFemaleSVG.svg";
+import PPGenderless from "../images/Profile/PPGenderless.png";
+
+export function removeOneProp(object, propToBeRemoved) {
+  const copyObj = { ...object };
+  delete copyObj[propToBeRemoved];
+  return copyObj;
+};
+
+export function setUserDocument(docTitle, docData) {
+  return firestore.collection("users").doc(docTitle).set(docData);
+};
+
+export function createInterestString(interests) {
+  let interestsString = "";
+  interests.forEach((interestObj, index, array) => {
+    if (index === array.length - 1) {
+      interestsString += `${interestObj.content}.`;
+      return;
+    }
+    interestsString += `${interestObj.content} | `;
+  });
+  return interestsString;
+};
+
+export function getDefaultGenderImage(gender) {
+  switch (gender) {
+    case "Prefer not to say":
+      return PPGenderless;
+    case "Male":
+      return PPMaleSVG;
+    case "Female":
+      return PPFemaleSVG;
+    default:
+      return undefined;
+  }
+};
