e136373fd6cb4ac472112bf97ad150e494069e16
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-01 16:53:23 +0300

    Add the store to the provider

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-
 import { StyleSheet, View, NativeModules, Platform, LogBox } from 'react-native';
 
 import Routes from './navigation';
 import { enableScreens } from 'react-native-screens';
+
+import { Provider } from 'react-redux';
+import { store } from  "./redux/store";
 
 const { StatusBarManager } = NativeModules;
 const statusBarHeight = (Platform.OS === "android" && StatusBarManager.HEIGHT) || (Platform.OS === "ios" && StatusBarManager.getHeight(height => height));
@@ -20,10 +22,12 @@ export default function App() {
   }, [])
 
   return (
-    <View style={styles.container}>
-      <StatusBar />
-      <Routes />  
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <StatusBar />
+        <Routes />  
+      </View>
+    </Provider>
   );
 }
 
