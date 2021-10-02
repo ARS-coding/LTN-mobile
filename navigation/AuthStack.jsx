19d8a1f2f15acc127a42e3d0ecb8d4d9894551a3
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-02 17:12:56 +0300

    Make the sign in screen first on auth stack

--- navigation/AuthStack.jsx
+++ navigation/AuthStack.jsx
@@ -12,8 +12,8 @@ const { Navigator, Screen } = Stack;
 export default function AuthStack() {
     return (
         <Navigator screenOptions={{headerShown: false}}>
-            <Screen name="SignUp" component={SignUpScreen} />
             <Screen name="Login" component={LoginScreen} />
+            <Screen name="SignUp" component={SignUpScreen} />
         </Navigator>
     );
 }
