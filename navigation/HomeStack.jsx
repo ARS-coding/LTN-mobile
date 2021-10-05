56c371954b843dbc562748ba3bde062e99bceeb3
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-05 12:45:43 +0300

    Customize the navigation bar that appears on the home screen

--- navigation/HomeStack.jsx
+++ navigation/HomeStack.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
+import { StyleSheet, View } from "react-native";
+import { IconButton } from "../components";
+
+import { auth } from "../config/firebase";
 
 import { createStackNavigator } from '@react-navigation/stack';
-
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
 const { Navigator, Screen } = Stack;
 
 export default function HomeStack() {
+    async function handleSignOut() {
+        try {
+           await auth.signOut();
+        } catch(error) {
+            console.error("An error has occured while trying to sign out:", error);
+        }
+    }
+    
     return (
-        <Navigator screenOptions={{headerShown: false}}>
-            <Screen name="Home" component={HomeScreen} />
+        <Navigator>
+            <Screen 
+                name="Home"
+                component={HomeScreen}
+                options={{headerRight: () => (
+                    <View style={styles.logoutIcon}>
+                        <IconButton 
+                            name="logout"
+                            size={24}
+                            onPress={handleSignOut}
+                        />
+                    </View>
+                )}}
+            />
         </Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    logoutIcon: {
+        transform: [{ rotate: "180deg"}],
+        marginRight: 12
+    },
+})
