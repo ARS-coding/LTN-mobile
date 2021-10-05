c8eba6852f5fdef222d1d014aef497a25dedf327
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-05 13:41:34 +0300

    Refactor the layout of sign up screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import {
     View,
+    ScrollView,
     Text,
     Button as RNButton,
     StyleSheet,
@@ -62,129 +63,131 @@ export default function SignUpScreen({ navigation }) {
     }
 
     return (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            <View style={styles.container}>
-                <Text style={styles.title}>
-                    Create a{" "}
-                    <Text style={{textDecorationLine: "underline"}}>
-                        New
-                    </Text> 
-                    {" "}Account!
-                </Text>
-                <View style={{ marginTop: 30 }}>
-                    <View style={{flexDirection: "row"}}>
+        <ScrollView style={styles.container}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <>
+                    <Text style={styles.title}>
+                        Create a{" "}
+                        <Text style={{textDecorationLine: "underline"}}>
+                            New
+                        </Text> 
+                        {" "}Account!
+                    </Text>
+                    <View style={{ marginTop: 30 }}>
+                        <View style={{flexDirection: "row"}}>
+                            <InputField
+                                containerStyle={{ flex: 1, marginBottom: 12, backgroundColor: "white" }}
+                                inputStyle={{ marginLeft: 8, fontSize: 14 }}
+                                placeholder="First Name"
+                                autoFocus={true}
+                                onChangeText={(text) => setFormData({ ...formData, firstName: text })}
+                                value={formData.firstName}
+                            />
+                            <InputField
+                                containerStyle={{ flex: 1, marginBottom: 12, marginLeft: 8, backgroundColor: "white" }}
+                                inputStyle={{ marginLeft: 8, fontSize: 14 }}
+                                placeholder="Last Name"
+                                onChangeText={(text) => setFormData({ ...formData, lastName: text })}
+                                value={formData.lastName}
+                            />
+                        </View>
+                        <View style={styles.picker}>
+                            <Picker 
+                                selectedValue={formData.gender} 
+                                onValueChange={gender => setFormData({ ...formData, gender: gender })}
+                                style={{ flex: 1 }}
+                            >
+                                <Picker.Item 
+                                    style={{ fontSize: 14 }}
+                                    label="Gender" 
+                                    value={null}
+                                />
+                                <Picker.Item style={{ fontSize: 14 }} label="Male" value="Male" />
+                                <Picker.Item style={{ fontSize: 14 }} label="Female" value="Female" />
+                                <Picker.Item style={{ fontSize: 14 }} label="Prefer not to say" value="Prefer not to say" />
+                            </Picker>
+                        </View>
+                        <View style={styles.picker}>
+                            <Picker 
+                                selectedValue={formData.district} 
+                                onValueChange={district => setFormData({ ...formData, district: district })}
+                                style={{ flex: 1 }}
+                            >
+                                <Picker.Item 
+                                    style={{ fontSize: 14 }} 
+                                    label="District"
+                                    value={null}
+                                />
+                                {arrayOfDistrictObjects.map((districtObject) => {
+                                    return (
+                                        <Picker.Item
+                                            key={districtObject.id}
+                                            style={{fontSize: 14}} 
+                                            label={districtObject.content}
+                                            value={districtObject.content}
+                                        />
+                                    )
+                                })}
+                            </Picker>
+                        </View>
                         <InputField
-                            containerStyle={{ flex: 1, marginBottom: 12, backgroundColor: "white" }}
-                            inputStyle={{ marginLeft: 8, fontSize: 14 }}
-                            placeholder="First Name"
-                            autoFocus={true}
-                            onChangeText={(text) => setFormData({ ...formData, firstName: text })}
-                            value={formData.firstName}
+                            containerStyle={{ marginBottom: 12, backgroundColor: "white" }}
+                            inputStyle={{ fontSize: 14 }}
+                            placeholder="Email"
+                            leftIcon="email"
+                            autoCapitalize="none"
+                            keyboardType="email-address"
+                            
+                            onChangeText={(text) => setFormData({ ...formData, email: text })}
+                            value={formData.email}
                         />
                         <InputField
-                            containerStyle={{ flex: 1, marginBottom: 12, marginLeft: 8, backgroundColor: "white" }}
-                            inputStyle={{ marginLeft: 8, fontSize: 14 }}
-                            placeholder="Last Name"
-                            onChangeText={(text) => setFormData({ ...formData, lastName: text })}
-                            value={formData.lastName}
+                            containerStyle={{ marginBottom: 12, backgroundColor: "white" }}
+                            inputStyle={{ fontSize: 14 }}
+                            placeholder="Password"
+                            leftIcon="lock"
+                            rightIcon={passwordInputs.passwordInput.icon}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            secureTextEntry={passwordInputs.passwordInput.isNotVisible}
+                            rightIconOnPress={() => handlePasswordVisibility("passwordInput")}
+                            
+                            onChangeText={(text) => setFormData({ ...formData, password: text })}
+                            value={formData.password}
+                        />
+                        <InputField
+                            containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
+                            inputStyle={{ fontSize: 14 }}
+                            placeholder="Confirm password"
+                            leftIcon="lock"
+                            rightIcon={passwordInputs.passwordConfirmationInput.icon}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            secureTextEntry={passwordInputs.passwordConfirmationInput.isNotVisible}
+                            rightIconOnPress={() => handlePasswordVisibility("passwordConfirmationInput")}
+                            
+                            onChangeText={(text) => setFormData({ ...formData, passwordConfirmation: text })}
+                            value={formData.confirmationPassword}
                         />
                     </View>
-                    <View style={styles.picker}>
-                        <Picker 
-                            selectedValue={formData.gender} 
-                            onValueChange={gender => setFormData({ ...formData, gender: gender })}
-                            style={{ flex: 1 }}
-                        >
-                            <Picker.Item 
-                                style={{ fontSize: 14 }}
-                                label="Gender" 
-                                value={null}
-                            />
-                            <Picker.Item style={{ fontSize: 14 }} label="Male" value="Male" />
-                            <Picker.Item style={{ fontSize: 14 }} label="Female" value="Female" />
-                            <Picker.Item style={{ fontSize: 14 }} label="Prefer not to say" value="Prefer not to say" />
-                        </Picker>
+                    {(signUpError !== "") && <ErrorMessage
+                        error={signUpError}
+                        visible={true}
+                    />}
+                    <View>
+                        <Button 
+                            title="Sign Up"
+                            containerStyle={{ marginBottom: 12 }}
+                            onPress={handleSignUp}    
+                        />
+                        <RNButton 
+                            title="Go to Login" 
+                            onPress={() => navigation.navigate('Login')}
+                        />
                     </View>
-                    <View style={styles.picker}>
-                        <Picker 
-                            selectedValue={formData.district} 
-                            onValueChange={district => setFormData({ ...formData, district: district })}
-                            style={{ flex: 1 }}
-                        >
-                            <Picker.Item 
-                                style={{ fontSize: 14 }} 
-                                label="District"
-                                value={null}
-                            />
-                            {arrayOfDistrictObjects.map((districtObject) => {
-                                return (
-                                    <Picker.Item
-                                        key={districtObject.id}
-                                        style={{fontSize: 14}} 
-                                        label={districtObject.content}
-                                        value={districtObject.content}
-                                    />
-                                )
-                            })}
-                        </Picker>
-                    </View>
-                    <InputField
-                        containerStyle={{ marginBottom: 12, backgroundColor: "white" }}
-                        inputStyle={{ fontSize: 14 }}
-                        placeholder="Email"
-                        leftIcon="email"
-                        autoCapitalize="none"
-                        keyboardType="email-address"
-                        
-                        onChangeText={(text) => setFormData({ ...formData, email: text })}
-                        value={formData.email}
-                    />
-                    <InputField
-                        containerStyle={{ marginBottom: 12, backgroundColor: "white" }}
-                        inputStyle={{ fontSize: 14 }}
-                        placeholder="Password"
-                        leftIcon="lock"
-                        rightIcon={passwordInputs.passwordInput.icon}
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        secureTextEntry={passwordInputs.passwordInput.isNotVisible}
-                        rightIconOnPress={() => handlePasswordVisibility("passwordInput")}
-                        
-                        onChangeText={(text) => setFormData({ ...formData, password: text })}
-                        value={formData.password}
-                    />
-                    <InputField
-                        containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
-                        inputStyle={{ fontSize: 14 }}
-                        placeholder="Confirm password"
-                        leftIcon="lock"
-                        rightIcon={passwordInputs.passwordConfirmationInput.icon}
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        secureTextEntry={passwordInputs.passwordConfirmationInput.isNotVisible}
-                        rightIconOnPress={() => handlePasswordVisibility("passwordConfirmationInput")}
-                        
-                        onChangeText={(text) => setFormData({ ...formData, passwordConfirmation: text })}
-                        value={formData.confirmationPassword}
-                    />
-                </View>
-                {(signUpError !== "") && <ErrorMessage
-                    error={signUpError}
-                    visible={true}
-                />}
-                <View>
-                    <Button 
-                        title="Sign Up"
-                        containerStyle={{ marginBottom: 12 }}
-                        onPress={handleSignUp}    
-                    />
-                    <RNButton 
-                        title="Go to Login" 
-                        onPress={() => navigation.navigate('Login')}
-                    />
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
+                </>
+            </TouchableWithoutFeedback>
+        </ScrollView>
     )
 }
 
