859d7e52f4b8110cf32cea67502dd8d7000aa29a
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-05 13:40:42 +0300

    Wrap the whole app in KeyboardAvoidingView and TouchableWithoutFeedback

--- App.jsx
+++ App.jsx
@@ -5,8 +5,16 @@ import { useDispatch, Provider } from 'react-redux';
 import { listenForAuthChanges } from './redux/slices/userSlice';
 import { store } from "./redux/store";
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, NativeModules, Platform, LogBox } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  NativeModules,
+  Platform,
+  Keyboard,
+  LogBox
+} from 'react-native';
 
 import Routes from './navigation/RootNavigator';
 import { enableScreens } from 'react-native-screens';
@@ -33,16 +41,23 @@ function App() {
   }, [])
 
   return (
-    <View style={styles.container}>
-      <StatusBar />
-      <Routes />  
-    </View>
+    <KeyboardAvoidingView 
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <>
+          {/* <StatusBar /> */}
+          <Routes />  
+        </>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: statusBarHeight,
+    // marginTop: statusBarHeight,
   },
 });
