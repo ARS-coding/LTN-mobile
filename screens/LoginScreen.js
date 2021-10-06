ee77df823848e28c4323d70fdb81955578758667
Ali Rıza <ars.lifestyle@hotmail.com>
2021-10-06 23:39:26 +0300

    Remove unnecessary TouchableWithoutFeedback component in the login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,12 +2,9 @@ import React, { useState } from 'react'
 
 import {
     StyleSheet,
-    View,
     ScrollView,
     Text,
     Button as RNButton,
-    TouchableWithoutFeedback,
-    Keyboard
 } from "react-native";
 import { Button, InputField, ErrorMessage } from "../components"
 
@@ -41,50 +38,48 @@ export default function LoginScreen({ navigation }) {
 
     return (
         <ScrollView style={styles.container}>
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <>
-                    <Text style={styles.title}>Login</Text>
-                    <InputField 
-                        containerStyle={{ marginTop: 20, marginBottom: 12, backgroundColor: "white" }}
-                        inputStyle={{ fontSize: 14 }}
-                        leftIcon="email"
-                        placeholder="Email"
-                        autoCapitalize="none"
-                        keyboardType="email-address"
-                        autoFocus={true}
+            <>
+                <Text style={styles.title}>Login</Text>
+                <InputField 
+                    containerStyle={{ marginTop: 20, marginBottom: 12, backgroundColor: "white" }}
+                    inputStyle={{ fontSize: 14 }}
+                    leftIcon="email"
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    autoFocus={true}
 
-                        onChangeText={(text) => setFormdata({ ...formData, email: text })}
-                        value={formData.email}
-                    />
-                    <InputField 
-                        containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
-                        inputStyle={{ fontSize: 14 }}
-                        leftIcon='lock'
-                        rightIcon={passwordInput.icon}
-                        placeholder="Password"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        secureTextEntry={passwordInput.isNotVisible}
-                        rightIconOnPress={handlePasswordVisibility}
+                    onChangeText={(text) => setFormdata({ ...formData, email: text })}
+                    value={formData.email}
+                />
+                <InputField 
+                    containerStyle={{ marginBottom: 20, backgroundColor: "white" }}
+                    inputStyle={{ fontSize: 14 }}
+                    leftIcon='lock'
+                    rightIcon={passwordInput.icon}
+                    placeholder="Password"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry={passwordInput.isNotVisible}
+                    rightIconOnPress={handlePasswordVisibility}
 
-                        onChangeText={(text) => setFormdata({ ...formData, password: text })}
-                        value={formData.password}
-                    />
-                    {(loginError !== "") && <ErrorMessage
-                        error={loginError}
-                        visible={true}
-                    />}
-                    <Button 
-                        title="Login"
-                        onPress={() => { setLoginError(""); onLogin()}}
-                        containerStyle={{ marginBottom: 10 }}
-                    />
-                    <RNButton 
-                        title="Go to Sign Up"
-                        onPress={() => navigation.navigate('SignUp')}
-                    /> 
-                </>
-            </TouchableWithoutFeedback>
+                    onChangeText={(text) => setFormdata({ ...formData, password: text })}
+                    value={formData.password}
+                />
+                {(loginError !== "") && <ErrorMessage
+                    error={loginError}
+                    visible={true}
+                />}
+                <Button 
+                    title="Login"
+                    onPress={() => { setLoginError(""); onLogin()}}
+                    containerStyle={{ marginBottom: 10 }}
+                />
+                <RNButton 
+                    title="Go to Sign Up"
+                    onPress={() => navigation.navigate('SignUp')}
+                /> 
+            </>
         </ScrollView>
     )
 }
