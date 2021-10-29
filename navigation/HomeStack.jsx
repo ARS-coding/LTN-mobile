b734631b62ad024f9ca65057619e80532d92adfe
Ali Rıza Şahin <arsahin.dev@gmail.com>
2021-10-29 19:54:11 +0300

    Declare prop types of HomeStack components

--- navigation/HomeStack.jsx
+++ navigation/HomeStack.jsx
@@ -1,46 +1,94 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+
 import { StyleSheet, View } from "react-native";
 import { IconButton } from "../components";
 
 import { auth } from "../config/firebase";
 
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/ProfileScreen';
+import { connect } from "react-redux";
+import {
+  editProfile,
+  saveProfileChanges,
+  discardProfileChanges,
+} from "../redux/slices/userSlice";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import ProfileScreen from "../screens/ProfileScreen";
 
 const Stack = createStackNavigator();
 const { Navigator, Screen } = Stack;
 
-export default function HomeStack() {
-    async function handleSignOut() {
-        try {
-           await auth.signOut();
-        } catch(error) {
-            console.error("An error has occured while trying to sign out:", error);
-        }
+function HomeStack({
+  isEditingProfile,
+  editProfile,
+  saveProfileChanges,
+  discardProfileChanges,
+}) {
+  async function handleSignOut() {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.error("An error has occured while trying to sign out:", error);
     }
-    
-    return (
-        <Navigator>
-            <Screen 
-                name="Home"
-                component={HomeScreen}
-                options={{headerRight: () => (
-                    <View style={styles.logoutIcon}>
-                        <IconButton 
-                            name="logout"
-                            size={24}
-                            onPress={handleSignOut}
-                        />
-                    </View>
-                )}}
-            />
-        </Navigator>
-    )
+  }
+
+  return (
+    <Navigator>
+      <Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{
+          headerRight: () => (
+            <View>
+              <View>
+                {isEditingProfile ? (
+                  <>
+                    <IconButton
+                      name="checkcircleo"
+                      size={24}
+                      onPress={saveProfileChanges}
+                    />
+                    <IconButton
+                      name="closecircleo"
+                      size={24}
+                      onPress={discardProfileChanges}
+                    />
+                  </>
+                ) : (
+                  <IconButton name="edit" size={24} onPress={editProfile} />
+                )}
+              </View>
+
+              <View style={styles.logoutIcon}>
+                <IconButton name="logout" size={24} onPress={handleSignOut} />
+              </View>
+            </View>
+          ),
+        }}
+      />
+    </Navigator>
+  );
 }
 
 const styles = StyleSheet.create({
-    logoutIcon: {
-        transform: [{ rotate: "180deg"}],
-        marginRight: 12
-    },
-})
+  logoutIcon: {
+    transform: [{ rotate: "180deg" }],
+    marginRight: 12,
+  },
+});
+
+HomeStack.propTypes = {
+  isEditingProfile: PropTypes.bool.isRequired,
+  editProfile: PropTypes.func.isRequired,
+  saveProfileChanges: PropTypes.func.isRequired,
+  discardProfileChanges: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ isEditingProfile }) => ({ isEditingProfile });
+const mapDispacthToProps = {
+  editProfile,
+  saveProfileChanges,
+  discardProfileChanges,
+};
+export default connect(mapStateToProps, mapDispacthToProps)(HomeStack);
